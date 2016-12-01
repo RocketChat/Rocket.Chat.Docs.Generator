ffc0cb1bf79d5382f77e5aea6ec8731546e08594
Diego Sampaio <chinello@gmail.com>
2016-12-01 15:25:55 -0200

    Add order support

--- plugins/generate-menu.js
+++ plugins/generate-menu.js
@@ -2,17 +2,22 @@ var _ = require('underscore');
 var slugifyPath = require('slugify-path').default;
 var path = require('path');
 
-function createTree(fileTree, items) {
+function createTree(fileTree, items, order) {
 	var item = items.splice(0, 1);
 
 	if (items.length > 0) {
-
 		if (!fileTree[item]) {
-			fileTree[item] = {};
+			fileTree[item] = {
+				order: order,
+				children: {}
+			};
 		}
-		createTree(fileTree[item], items);
+		createTree(fileTree[item].children, items, order);
 	} else {
-		fileTree[item] = {};
+		fileTree[item] = {
+			order: order,
+			children: {}
+		};
 	}
 }
 
@@ -27,7 +32,7 @@ function createMenu(fileTree, initialItem, initialPath, level) {
 	}
 
 	Object.keys(fileTree).forEach(function(item) {
-		if (item === 'README.md' || (_.isEmpty(fileTree[item]) && !item.match(/\.md$/))) {
+		if (item === 'README.md' || (_.isEmpty(fileTree[item].children) && !item.match(/\.md$/))) {
 			return;
 		}
 
@@ -37,15 +42,23 @@ function createMenu(fileTree, initialItem, initialPath, level) {
 			linkClass = 'guide-nav-item';
 		}
 
+		var order = item.match(/^([0-9]+)\. /);
+		var defaultSort = '0';
+		if (order) {
+			defaultSort = order[1];
+		}
+		var itemOrder = (fileTree[item].order || defaultSort);
+		itemOrder = ('000000' + itemOrder).substr(-6);
+
 		var title = item.replace(/^[0-9]+\. /, '').replace(/\.md$/, '');
 		folders.push({
 			label: title,
-			sortTitle: initialItem + '-' + (item.match(/\.md$/) ? '' : item),
+			sortTitle: initialItem + '-' + (item.match(/\.md$/) ? '' : itemOrder + '+' + item),
 			link: initialPath + '/' + slugifyPath(title) + (item.match(/\.md$/) ? '' : '/'),
 			class: linkClass + levelClass
 		});
 
-		folders = folders.concat(createMenu(fileTree[item], initialItem + '-' + item, initialPath + '/' + slugifyPath(title), level + 1));
+		folders = folders.concat(createMenu(fileTree[item].children, initialItem + '-' + (item.match(/\.md$/) ? '' : itemOrder + '+' + item), initialPath + '/' + slugifyPath(title), level + 1));
 	});
 
 	return folders;
@@ -57,11 +70,10 @@ function generateMenu() {
 
 		Object.keys(files).forEach(function(file) {
 			var data = files[file];
-
 			var fileParts = data.originalName.split(path.sep);
 			var totalParts = fileParts.length;
 
-			createTree(fileTree, fileParts);
+			createTree(fileTree, fileParts, data.order || null);
 		});
 
 		var folders = createMenu(fileTree, '', '', 1);
