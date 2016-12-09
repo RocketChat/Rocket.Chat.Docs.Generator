0bd62c11bfba000f3abb051c17e89ac77214b99f
Diego Sampaio <chinello@gmail.com>
2016-12-09 14:11:19 -0200

    Use Senna API for navigation

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
-	var path = window.location.pathname;
+	setActiveMenu(window.location.pathname, false);
 
-	//Find the current active page and set it active, then scroll to it
-	$('.sidebar li a').each(function() {
-		var href = this.href;
-		if (_endsWith(href, path)) {
-			$(this).parent().addClass('active');
-			$('.sidebar').scrollTop($(this).offset().top - 68);
-		}
+	var app = new senna.App();
+	app.setBasePath('/docs');
+	app.addSurfaces('content');
+	app.addRoutes(new senna.Route(/.*/, senna.HtmlScreen));
+
+	app.on('startNavigate', function(event) {
+		setActiveMenu(event.path, true);
 	});
 
-	//Register the click on the first load, each additional pages will call the function theirselves
-	_registerAnchorClick();
-});
-
-$('.sidebar li a').on('click', function(e) {
-	$('.sidebar li.active').each(function() {
-		$(this).removeClass('active');
+	app.on('endNavigate', function(event) {
+		scrollContent(event.path);
 	});
-
-	$('base').attr('href', $(this).attr('href'));
-	$(this).parent().addClass('active');
 });
+
+function scrollContent(path) {
+	var hashPosition = path.indexOf('#');
+
+	if (hashPosition !== -1) {
+		location.hash = path.substr(hashPosition);
+	} else {
+		$('#content').scrollTop(0);
+	}
+}
 
 function _endsWith(item, suffix) {
 	return item.indexOf(suffix, item.length - suffix.length) !== -1;
@@ -35,15 +37,51 @@ function _registerAnchorClick() {
 		$('.sidebar li.active').each(function() {
 			$(this).removeClass('active');
 		});
-
-		//Find the list item anchor that matches where they're going 
-		$('.sidebar li a').each(function() {
-			if($(this).attr('href') === goingToHref) {
-				$(this).parent().addClass('active');
-			}
-		});
-
-
-		$('base').attr('href', goingToHref);
 	});
 };
+
+function setActiveMenu(path, animate) {
+	var hashPosition = path.indexOf('#');
+
+	if (hashPosition !== -1) {
+		path = path.substr(0, hashPosition);
+	}
+
+	$('.sidebar li.active').removeClass('active');
+
+	//Find the current active page and set it active, then scroll to it
+	$('.sidebar li a').each(function() {
+		var href = this.href;
+		// if (_endsWith(href, path)) {
+		if (_endsWith(href, path) || _endsWith(href, path + '/')) {
+			$(this).parent().addClass('active');
+
+			if (!isElementInViewport($(this).parent())) {
+				var position = $('.sidebar').scrollTop() + $(this).offset().top - 68;
+				if (animate) {
+					$('.sidebar').stop().animate({ scrollTop: position }, 500);
+				} else {
+					$('.sidebar').scrollTop(position);
+				}
+			}
+		}
+	});
+}
+
+function isElementInViewport(el) {
+
+	//special bonus for those using jQuery
+	if (typeof jQuery === "function" && el instanceof jQuery) {
+		el = el[0];
+	}
+
+	var rect = el.getBoundingClientRect();
+
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+	);
+}
+
